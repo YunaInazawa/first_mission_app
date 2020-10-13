2444ab625fe8fc4b01619a1610bbd5334ba9ea24
YamaguchiMayuko <2170035@ecccomp.com>
2020-10-13 13:40:39 +0900

    使用言語の項目追加、メンバー追加のリンクの削除

--- resources/views/app_create.blade.php
+++ resources/views/app_create.blade.php
@@ -1,7 +1,7 @@
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 100 100" preserveAspectRatio="none" id="svg-bg">
     <path d="M0,0 v50 q10,10 20,0 t20,0 t20,0 t20,0 t20,0 v-50 Z" fill="#FFF"></path>
 </svg>
-@extends('layouts.app')
+@extends('layouts.app_header')
 
 @section('content')
 
@@ -17,13 +17,19 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
-                    <div class="form-group row pjtname">
+                    <div class="form-group row name">
                         <label for="" class="col-lg-2 col-form-label text-md-right">{{ __('プロジェクト名') }}</label>
                         <div class="col-lg-9">
                             <input id="" type="text" class="form-control" name="" value="" autofocus>
                         </div>
                     </div>
-                    <div class="form-group row pjtgaiyo">
+                    <div class="form-group row using">
+                        <label for="" class="col-lg-2 col-form-label text-md-right">{{ __('使用言語・技術') }}</label>
+                        <div class="col-lg-9">
+                            <input id="" type="text" class="form-control" name="" value="" autofocus>
+                        </div>
+                    </div>
+                    <div class="form-group row description">
                         <label for="" class="col-lg-2 col-form-label text-md-right">{{ __('概要') }}</label>
                         <div class="col-lg-9">
                             <textarea class="form-control" name="example" cols="50" rows="10"></textarea>
@@ -32,7 +38,19 @@
                     <div class="form-group row menber">
                         <label for="" class="col-lg-2 col-form-label text-md-right">{{ __('メンバー') }}</label>
                         <div class="col-lg-9">
-                            <a href="{{ url('/create') }}" class="btn-circle-3d">＋</a>
+                            <div id="dialog">
+                                <div id="dialogBackground"></div>
+                                <div id="dialogContent">
+                                    <div id="dialogMsg">
+                                        ダイアログメッセージ
+                                    </div>
+                                    <div>
+                                        <input type="button" value="はい" onclick="func();" />
+                                        <input type="button" value="いいえ" onclick="dialogHide();" />
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="{{ url('/') }}" class="btn-circle-3d">＋</a>
                         </div>
                     </div>
                     <div class="form-group row">
