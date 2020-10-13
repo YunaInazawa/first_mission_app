a1a282c6aaf66613b859e32b13de3c537f44877b
sei <teatime.0704@gmail.com>
2020-10-13 16:50:02 +0900

    [add] app_create(DB登録)

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Project;
+use App\Member;
+use App\Role;
+use App\Log;
+use App\LogCategory;
 
 class AppController extends Controller
 {
@@ -10,27 +16,88 @@ class AppController extends Controller
     {
         $this->middleware('auth');
     }
-    
-    public function index( $id = 1 )
-    {
-        return view('app_home', ['app_id'=> $id]);
-    }
 
+    function createLog( $logText, $logCategory, $project_id )
+    {
+        $newLog = new Log;
+        $newLog->text = $logText;
+        $newLog->log_category_id = LogCategory::where('content', $logCategory)->first()->id;
+        $newLog->user_id = Auth::id();
+        $newLog->project_id = $project_id;
+        $newLog->save();
+
+    }
+    
+    /**
+     * プロジェクト作成
+     */
     public function create()
     {
         return view('app_create');
     }
 
+    /**
+     * プロジェクト作成 / DB登録（POST）
+     */
+    public function new( Request $request )
+    {
+        // 二重送信禁止
+        $request->session()->regenerateToken();  
+        
+        // データ取得
+        $projectName = $request->project_name;
+        $projectUsing = $request->project_using;
+        $projectDescription = $request->project_description;
+
+        // プロジェクト作成
+        $newProject = new Project;
+        $newProject->name = $projectName;
+        $newProject->description = $projectDescription;
+        $newProject->using = $projectUsing;
+        $newProject->save();
+        $this->createLog('ユーザ「' . Auth::user()->name . '」がプロジェクト「' . $newProject->name . '」を作成', 'create', $newProject->id);
+
+        // メンバ登録(作成者)
+        $newMember = new Member;
+        $newMember->is_join = true;
+        $newMember->project_id = $newProject->id;
+        $newMember->user_id = Auth::id();
+        $newMember->role_id = Role::where('name', '代表')->first()->id;
+        $newMember->save();
+
+        // メンバ登録(参加申請)
+
+        
+        return redirect(route('app_home', ['id' => $newProject->id]));
+    }
+
+    /**
+     * プロジェクト管理
+     */
+    public function index( $id = 1 )
+    {
+        return view('app_home', ['app_id'=> $id, 'data' => '']);
+    }
+
+    /**
+     * 画面管理
+     */
     public function screen( $id = 1 )
     {
         return view('task_screen', ['app_id'=> $id]);
     }
 
+    /**
+     * タスク管理
+     */
     public function detail( $id = 1 )
     {
         return view('task_detail', ['app_id'=> $id]);
     }
 
+    /**
+     * デザイン管理
+     */
     public function design( $id = 1 )
     {
         return view('design', ['app_id'=> $id]);
