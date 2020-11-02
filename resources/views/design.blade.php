4cbeb76339a80a4bd568263b25bd6b9980bf54e3
YamaguchiMayuko <2170035@ecccomp.com>
2020-11-02 13:54:41 +0900

    デザイン管理画面のデザインを調整して、タブ切り替えで、キャンバスを切り替え出来るようにした

--- resources/views/design.blade.php
+++ resources/views/design.blade.php
@@ -6,39 +6,71 @@
 @section('content')
 
 <!-- Stylesheet -->
-<link href="{{ asset('css/design.css') }}" rel="stylesheet">
 <link href="{{ asset('css/app_user.css') }}" rel="stylesheet">
-
+<link href="{{ asset('css/design.css') }}" rel="stylesheet">
 <div class="container">
     <div class="row">
         <div class="col-lg-2">
-            <aside id="sub">
-                <h2>オブジェクト</h2>
-                <ul>
-                    <li>Button</li>
-                    <li>RadioButton</li>
-                    <li>TextBox</li>
-                    <li>label</li>
-                    <li>checkBox</li>
-                    <li>screen</li>
-                </ul>
-            </aside>
+            <div class="row">
+                <div class="col-lg-2">
+                    <div id="nav-drawer">
+                        <input id="nav-input" type="checkbox" class="nav-unshown">
+                        <label id="nav-open" class="fas fa-bars" for="nav-input"></label>
+                        <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+                        <div id="nav-content">
+                            <span class="tab-trigger js-tab-trigger is-active" data-id="tab01">KINOKOLIST</span>
+                            <span class="tab-trigger js-tab-trigger" data-id="tab02">TOP</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-10">
+                    <aside class="sidemenu sidemenu_left">
+                        <h2>オブジェクト</h2>
+                            <button>
+                                <div class="row"><span class="col-lg-4 ic ic_btn">BUTTON</span><span class="col-lg-8 obj_name">Button</span></div>
+                            </button>
+                            <button>
+                                <div class="row"><span class="col-lg-4 ic ic_radio">●</span><span class="col-lg-8 obj_name">RadioButton</span></div>
+                            </button>
+                            <button>
+                                <div class="row"><span class="col-lg-4 ic ic_textb">abc |</span><span class="col-lg-8 obj_name">TextBox</span></div>
+                            </button>
+                            <button>
+                                <div class="row"><span class="col-lg-4 ic ic_label">label</span><span class="col-lg-8 obj_name">Label</span></div>
+                            </button>
+                            <button>
+                                <div class="row"><span class="col-lg-4 ic ic_checkb"><label class="fas fa-check"></label></span><span class="col-lg-8 obj_name">CheckBox</span></div>
+                            </button>
+                            <button>
+                                <div class="row"><span class="col-lg-4 ic ic_screen"></span><span class="col-lg-8 obj_name">Screen</span></div>
+                            </button> 
+                    </aside>
+                </div>
+            </div>
         </div>
         <div class="col-lg-8">
-            <div class="row">
-                <canvas class="can col-lg-12"></canvas>
+            <div class="tab-content">
+                <!-- 画面一覧の表示 -->
+                <div id="tab01" class="tab-content__item js-tab-target is-active">
+                    <canvas class="can col-lg-12"></canvas>
+                </div>
+                <!-- 画面遷移が表示される -->
+                <div id="tab02" class="tab-content__item js-tab-target">
+                    <canvas class="can col-lg-12" style="background-color:red;"></canvas>
+                </div>
             </div>
         </div>
         <div class="col-lg-2 ">
-            <aside id="sub2">
+            <aside class="sidemenu sidemenu_right">
                 <div class="row">
                     <h2 class="col-lg-12">詳細</h2>
+                    <hr class="col-lg-12 sidemenu_hr">
                     <p class="col-lg-3">名前</p><div class="v_line_fix"></div>
                     <input class="col-lg-8" type="text">
-                    <hr class="col-lg-12">
+                    <hr class="col-lg-12 sidemenu_hr">
                     <span class="col-lg-3">高さ</span><div class="v_line_fix"></div><input class="col-lg-2" type="text">
                     <span class="col-lg-3">幅</span><div class="v_line_fix"></div><input class="col-lg-2" type="text">
-                    <hr class="col-lg-12">
+                    <hr class="col-lg-12 sidemenu_hr">
                     <span class="col-lg-3">座標X</span><div class="v_line_fix"></div><input class="col-lg-2" type="text">
                     <span class="col-lg-3">座標Y</span><div class="v_line_fix"></div><input class="col-lg-2" type="text">
                 </div>
@@ -54,5 +86,6 @@
 
 <!-- Script -->
 <script src="{{ asset('js/dialog.js') }}"></script>
+<script src="{{ asset('js/tab.js') }}"></script>
 
 @endsection
