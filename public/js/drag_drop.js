860cfb01f20c13b9ca931a03167a66ef64a73eac
YamaguchiMayuko <2170035@ecccomp.com>
2020-11-19 09:35:18 +0900

    ドラッグで、オブジェクト追加可能に。右メニューに現在存在してるオブジェクトを画面ごとにツリー表示

--- public/js/drag_drop.js
+++ public/js/drag_drop.js
@@ -14,9 +14,51 @@
 // 右メニューのinnputオブジェクトを全て取得(名前、文字、高さ、幅、ｘ、ｙ)
 const elms = document.querySelectorAll('.js-input-elm');
 // 追加要素の共通部分格納
-let tmpObj = '<div onclick="ObjClick(event)" ondrag="Drag(event)" draggable="true" ondragstart="DragStart(event)"  style="top:0;left:0;font-size:18px;" class="draggable obj ';
-
-
+let tmpObj = '<div onclick="ObjClick(event)" ondrag="Drag(event)" draggable="true" ondragstart="DragStart(event)" style="top:0;left:0;font-size:18px;" class="draggable obj ';
+var droptmpObj ='<div onclick="ObjClick(event)" ondrag="Drag(event)" draggable="true" ondragstart="DragStart(event)" class="draggable obj ';
+let alreadyTmpObj = '<div onclick="ObjClick(event)" ondrag="Drag(event)" draggable="true" ondragstart="DragStart(event)" class="draggable obj ';
+let objListItem = '<li><a onclick="TreeClick(event)" class="obj_list_item" id="obj_list_';
+var dropObjId = "";
+var startY = 0;
+var startX = 0;
+var endY = 0;
+var endX = 0;
+StartFunc();
+function StartFunc(){
+    // '.canvas'を持つ要素を取得
+    const tabTargets = document.querySelectorAll('.canvas');
+    let key = Object.keys(object);
+    for (var k in key) {
+        var objitemset = '';
+        var styleSet = 'style="top: ' + object[key[k]]['y'] + 'px; left: ' + object[key[k]]['x'] + 'px; font-size: 18px; height: ' + object[key[k]]['height'] + 'px; width: ' + object[key[k]]['width'] + 'px;"';
+        if(object[key[k]]['element'] == "Buttom"){
+            tabTargets[parseInt(object[key[k]]['screen'])].innerHTML += alreadyTmpObj + 'obj_btn"' + styleSet +' id="obj_already_btn_' + key[k] + '">' + object[key[k]]['name'] + '</div>';
+            objitemset = objListItem + 'alr_btn_' + key[k] + '">' + object[key[k]]['element'] + key[k] + '-<span id="list_alr_btn_' + key[k] + '">' + object[key[k]]['name'] + '</span>';
+        }else if(object[key[k]]['element'] == "CheckBox"){
+            tabTargets[parseInt(object[key[k]]['screen'])].innerHTML += alreadyTmpObj + 'obj_checkbox"' + styleSet +' id="obj_already_checkbox_' + key[k] + '"><input id="already_checkbox_' + key[k] + '" type="checkbox">' + object[key[k]]['name'] + '</div>';
+            objitemset = objListItem + 'alr_checkbox_' + key[k] + '">' + object[key[k]]['element'] + key[k] + '-<span id="list_alr_checkbox_' + key[k] + '">' + object[key[k]]['name'] + '</span>';
+        }else if(object[key[k]]['element'] == "Label"){
+            tabTargets[parseInt(object[key[k]]['screen'])].innerHTML += alreadyTmpObj + 'obj_label"' + styleSet +' id="obj_already_label_' + key[k] + '">' + object[key[k]]['name'] + '</div>';
+            objitemset = objListItem + 'alr_label_' + key[k] + '">' + object[key[k]]['element'] + key[k] + '-<span id="list_alr_label_' + key[k] + '">' + object[key[k]]['name'] + '</span>';
+        }else if(object[key[k]]['element'] == "RadioButtom"){
+            tabTargets[parseInt(object[key[k]]['screen'])].innerHTML += alreadyTmpObj + 'obj_radio"' + styleSet +' id="obj_already_radio_' + key[k] + '"><input id="already_radio_' + key[k] + '" type="radio">' + object[key[k]]['name'] + '</div>';
+            objitemset = objListItem + 'alr_radio_' + key[k] + '">' + object[key[k]]['element'] + key[k] + '-<span id="list_alr_radio_' + key[k] + '">' + object[key[k]]['name'] + '</span>';
+        }else if(object[key[k]]['element'] == "TextBox"){
+            tabTargets[parseInt(object[key[k]]['screen'])].innerHTML += alreadyTmpObj + 'obj_textbox"' + styleSet +' id="obj_already_textbox_' + key[k] + '"><input id="already_textbox_' + key[k] + '" type="text"></div>';
+            objitemset = objListItem + 'alr_textbox_' + key[k] + '">' + object[key[k]]['element'] + key[k];
+        }
+        document.getElementById('links'+object[key[k]]['s']).innerHTML += objitemset + '</a></li>';
+    }
+}
+function AddObjList(addobj,addid){
+    const tabTargets = document.querySelectorAll('.js-tab-target');
+    var num = document.getElementById( "select_screen").value;
+    var objitemset = objListItem + addid + '_' + document.getElementById('id_new').value + '"><span class="new_c">●</span><span class="new_p">＋</span>' + addobj + document.getElementById('id_new').value;
+    if(addobj != "TextBox"){
+        objitemset += '-<span id="list_' + addid +'_' + document.getElementById('id_new').value +'">' + addobj + '</span>';
+    }
+    document.getElementById('links' + (tabTargets[num].id).replace("tab0","")).innerHTML += objitemset + '</a></li>';
+}
 
 /*****************************************
  * 2.ドラッグ&ドロップ
@@ -27,23 +69,87 @@ let tmpObj = '<div onclick="ObjClick(event)" ondrag="Drag(event)" draggable="tru
 // ドラッグ開始時にデータの値を設定する
 function DragStart(event) {
     event.dataTransfer.setData("text", event.target.id);
-    SelectSet(event); // オブジェクトの情報を取得し、右メニューに表示
+    SelectSet(event.target.id); // オブジェクトの情報を取得し、右メニューに表示
+    startY = event.clientY;
+    startX = event.clientX;
 }
 // ドラッグ中位置情報を常に表示させる
 function Drag(event) {
-    elms[4].value = event.clientX - 383;
-    elms[5].value = event.clientY - 87;
+    endX = event.clientX;
+    endY = event.clientY;
+    elms[4].value = (event.target.style.left).replace("px","") - (startX - endX);
+    elms[5].value = (event.target.style.top).replace("px","") - (startY - endY);
 }
 // ドロップ時に元のドラッグ値を取得し、現在の要素上に入れ子で保存する
 function Drop(event) {
     var id = event.dataTransfer.getData("text");
+    if(dropObjId != ""){
+        DropAdd(dropObjId);
+        dropObjId = "";
+        return;
+    }
     var elm = document.getElementById(id);
+    var x = event.clientX;
+    var y = event.clientY;
+    var width = event.target.offsetLeft;
+    var height =event.target.offsetTop;
+    // 要素の位置座標を取得
+    var clientRect = event.target.getBoundingClientRect() ;
+    var clientRect2 = document.getElementById(id).getBoundingClientRect() ;
 
+    // 画面の左端から、要素の左端までの距離
+    var xxx = clientRect2.left ;
+    
+    // 画面の上端から、要素の上端までの距離
+    var yyy = clientRect2.top ;    // 画面の左端から、要素の左端までの距離
+    var xx = clientRect.left ;
+    
+    // 画面の上端から、要素の上端までの距離
+    var yy = clientRect.top ;
+    //alert("x:" + xxx + ",y:" + yyy + ",w:" + xx + ",h:" + yy);
     document.getElementById(id).classList.add('is-drop');
-    document.getElementById(id).style.top = (event.clientY - 87) + "px" ;
-    document.getElementById(id).style.left = (event.clientX - 383) + "px" ;
+    document.getElementById(id).style.top = ((document.getElementById(id).style.top).replace("px","") - (startY - endY)) + "px" ;
+    document.getElementById(id).style.left = ((document.getElementById(id).style.left).replace("px","") - (startX - endX)) + "px" ;
     event.currentTarget.appendChild(elm);
     event.preventDefault();
+}
+function DrapAddStart(event) {
+    event.target.classList.add('active');
+    dropObjId = event.target.id;
+}
+function DragAddEnd(event){
+    
+    event.target.classList.remove('active');
+}
+function DropAdd(id) {
+    // '.canvas'を持つ要素を取得
+    const tabTargets = document.querySelectorAll('.canvas');
+    var num = document.getElementById( "select_screen").value;
+    var styleSet = 'style="top:' + (event.clientY - 87) + 'px;left:' + (event.clientX - 383) + 'px;font-size:18px;"';
+    // drop_radio → obj_radio_0
+    if(dropObjId.indexOf("btn") != -1){
+        tabTargets[num].innerHTML += droptmpObj + 'obj_btn" id="obj_btn_' + document.getElementById('id_new').value + '"' + styleSet + '>Button</div>';
+        AddObjList('Buttom','btn');
+        SelectSet('obj_btn_' + document.getElementById('id_new').value);
+    }else if(dropObjId.indexOf("checkbox") != -1){
+        tabTargets[num].innerHTML += droptmpObj + 'obj_checkbox" id="obj_checkbox_' + document.getElementById('id_new').value + '"' + styleSet + '><input id="checkbox_1" type="checkbox">CheckBox</div>';
+        AddObjList('CheckBox','checkbox');
+        SelectSet('obj_checkbox_' + document.getElementById('id_new').value);
+    }else if(dropObjId.indexOf("label") != -1){
+        tabTargets[num].innerHTML += droptmpObj + 'obj_label" id="obj_label_' + document.getElementById('id_new').value +'"' + styleSet + '>Label</div>';
+        AddObjList('Label','label');
+        SelectSet('obj_label_' + document.getElementById('id_new').value);
+    }else if(dropObjId.indexOf("radio") != -1){
+        tabTargets[num].innerHTML += droptmpObj + 'obj_radio" id="obj_radio_' + document.getElementById('id_new').value + '"' + styleSet + '><input id="radio_1" type="radio">RadioButton</div>';
+        AddObjList('RadioButtom','radio');
+        SelectSet('obj_radio_' + document.getElementById('id_new').value);
+    }else if(dropObjId.indexOf("textbox") != -1){
+        tabTargets[num].innerHTML += droptmpObj + 'obj_textbox" id="obj_textbox_' + document.getElementById('id_new').value + '"' + styleSet + '><input id="textbox_1" type="text"></div>';
+        AddObjList('TextBox','textbox');
+        SelectSet('obj_textbox_' + document.getElementById('id_new').value);
+    }
+    document.getElementById('id_new').value = Number(document.getElementById('id_new').value) + 1;
+    
 }
 // ブラウザ標準のドロップ動作をキャンセル
 function DragOver(event) {
@@ -51,17 +157,19 @@ function DragOver(event) {
 }
 // オブジェクトをクリックしたときに、情報取得表示
 function ObjClick(event) {
-    SelectSet(event);
+    SelectSet(event.target.id);
 }
 
 /*
  * 2.2 選択オブジェクトの情報取得・表示用関数
  *****************************************/
-function SelectSet(event){
-    var selObjId = event.target.id;
+function SelectSet(id){
+    var selObjId = id;
     if(-1 == selObjId.indexOf('obj')){
         selObjId = "obj_" + selObjId;
     }
+    selObjId = selObjId.replace("list_","");
+    selObjId = selObjId.replace("alr_","already_");
     var selObj = document.getElementById(selObjId);
 
     // 選択したオブジェクトのIDを取得
@@ -112,40 +220,45 @@ function ObjBtnClick(event) {
     // '.canvas'を持つ要素を取得
     const tabTargets = document.querySelectorAll('.canvas');
     var num = document.getElementById( "select_screen").value;
-    $(tabTargets[num]).append(tmpObj + 'obj_btn" id="obj_btn_' + document.getElementById('id_btn').value + '">BUTTON</div>');
-    document.getElementById('id_btn').value = Number(document.getElementById('id_btn').value) + 1;
+    $(tabTargets[num]).append(tmpObj + 'obj_btn" id="obj_btn_' + document.getElementById('id_new').value + '">' + event.target.innerText + '</div>');
+    AddObjList('Buttom','btn');
+    document.getElementById('id_new').value = Number(document.getElementById('id_new').value) + 1;
 }
 
 function ObjLabelClick(event) {
     // '.canvas'を持つ要素を取得
     const tabTargets = document.querySelectorAll('.canvas');
     var num = document.getElementById( "select_screen").value;
-    $(tabTargets[num]).append(tmpObj + 'obj_label" id="obj_label_' + document.getElementById('id_label').value +'">label</div>');
-    document.getElementById('id_label').value = Number(document.getElementById('id_label').value) + 1;
+    $(tabTargets[num]).append(tmpObj + 'obj_label" id="obj_label_' + document.getElementById('id_new').value +'">' + event.target.innerText + '</div>');
+    AddObjList('Label','label');
+    document.getElementById('id_new').value = Number(document.getElementById('id_new').value) + 1;
 }
 
 function ObjRadioClick(event) {
     // '.canvas'を持つ要素を取得
     const tabTargets = document.querySelectorAll('.canvas');
     var num = document.getElementById( "select_screen").value;
-    $(tabTargets[num]).append(tmpObj + 'obj_radio" id="obj_radio_' + document.getElementById('id_radio').value + '"><input id="radio_1" type="radio">Radio</div>');
-    document.getElementById('id_radio').value = Number(document.getElementById('id_radio').value) + 1;
+    $(tabTargets[num]).append(tmpObj + 'obj_radio" id="obj_radio_' + document.getElementById('id_new').value + '"><input id="radio_1" type="radio">' + event.target.innerText + '</div>');
+    AddObjList('RadioButtom','radio');
+    document.getElementById('id_new').value = Number(document.getElementById('id_new').value) + 1;
 }
 
 function ObjTextBoxClick(event) {
     // '.canvas'を持つ要素を取得
     const tabTargets = document.querySelectorAll('.canvas');
     var num = document.getElementById( "select_screen").value;
-    $(tabTargets[num]).append(tmpObj + 'obj_textbox" id="obj_textbox_' + document.getElementById('id_text').value + '"><input id="textbox_1" type="text"></div>');
-    document.getElementById('id_text').value = Number(document.getElementById('id_text').value) + 1;
+    $(tabTargets[num]).append(tmpObj + 'obj_textbox" id="obj_textbox_' + document.getElementById('id_new').value + '"><input id="textbox_1" type="text"></div>');
+    AddObjList('TextBox','textbox');
+    document.getElementById('id_new').value = Number(document.getElementById('id_new').value) + 1;
 }
 
 function ObjCheckBoxClick(event) {
     // '.canvas'を持つ要素を取得
     const tabTargets = document.querySelectorAll('.canvas');
     var num = document.getElementById( "select_screen").value;
-    $(tabTargets[num]).append(tmpObj + 'obj_checkbox" id="obj_checkbox_' + document.getElementById('id_check').value + '"><input id="checkbox_1" type="checkbox">CheckBox</div>');
-    document.getElementById('id_check').value = Number(document.getElementById('id_check').value) + 1;
+    $(tabTargets[num]).append(tmpObj + 'obj_checkbox" id="obj_checkbox_' + document.getElementById('id_new').value + '"><input id="checkbox_1" type="checkbox">' + event.target.innerText + '</div>');
+    AddObjList('CheckBox','checkbox');
+    document.getElementById('id_new').value = Number(document.getElementById('id_new').value) + 1;
 }
 
 
@@ -158,10 +271,17 @@ function ChangeText(event){
     var evId = event.target.id;
     if(evId == 'obj-name'){
         document.getElementById(selObj).innerText = elms[0].value;
+        var sellist = '';
+        if(selObj.indexOf('already') != -1){
+            sellist = selObj.replace("obj_already","list_alr");
+        }else{
+            sellist = selObj.replace("obj","list");
+        }
+        document.getElementById(sellist).innerText = elms[0].value;
     }else if(evId == 'obj-height'){
         document.getElementById(selObj).style.height = elms[2].value + "px";
     }else if(evId == 'obj-width'){
-        document.getElementById(selObj).style.width = "10px";
+        document.getElementById(selObj).style.width = elms[3].value + "px";
     }else if(evId == 'obj-x'){
         document.getElementById(selObj).style.left = elms[4].value + "px";
     }else if(evId == 'obj-y'){
@@ -171,4 +291,19 @@ function ChangeText(event){
         elms[2].value = document.getElementById(selObj).clientHeight;
         elms[3].value = document.getElementById(selObj).clientWidth;
     }
+}
+
+
+function Check(event){
+    var checkac = (event.target.id).replace("menu_bar","links");
+    if(document.getElementById(checkac).classList == ""){
+        document.getElementById(checkac).classList.add('checked');
+    }else{
+        document.getElementById(checkac).classList.remove('checked');
+    }
+}
+
+
+function TreeClick(event){
+    SelectSet(event.target.id);
 }
--- resources/views/design.blade.php
+++ resources/views/design.blade.php
@@ -1,10 +1,39 @@
 <?php 
-$btn = 0;
-$radio = 0;
-$text = 0;
-$label = 0;
-$check = 0;
+$newid = 0;
+$tabfst = true;
+$tabtargetnum = 0;
+$objects = array();
+$screenID = 0;
+foreach( $scenes_data as $s ){
+    foreach( $decorations_data[$s->id] as $d ){
+        $objects += array(
+            $d->id => array(
+                'element' => $d->element->name,
+                'screen' => $screenID,
+                'name' => $d->text,
+                'height' => $d->height,
+                'width' => $d->width,
+                'x' => $d->position_x,
+                'y' => $d->position_y,
+                'fontsize' => '18',
+                's' => $s->id,
+            ),
+        );
+    }
+    $screenID = $screenID + 1;
+}
+
 ?>
+
+<?php
+$sample = array('abc','def');//PHPで配列を生成
+$varJsSample=json_encode($objects);//JavaScriptに渡すためにjson_encodeを行う
+?>
+
+<script type="text/javascript">
+var object=JSON.parse('<?php echo $varJsSample; ?>');//jsonをparseしてJavaScriptの変数に代入
+</script>
+
 @extends('layouts.app_header')
 
 @section('content')
@@ -22,8 +51,14 @@ $check = 0;
                         <label id="nav-open" class="fas fa-bars" for="nav-input"></label>
                         <label class="nav-unshown" id="nav-close" for="nav-input"></label>
                         <div id="nav-content">
-                            <span class="tab-trigger js-tab-trigger is-active" data-id="tab01">KINOKOLIST</span>
-                            <span class="tab-trigger js-tab-trigger" data-id="tab02">TOP</span>
+                            @foreach( $scenes_data as $s )
+                                @if($tabfst == true)
+                                    <span class="tab-trigger js-tab-trigger is-active" data-id="tab0<?php echo $s->id;$tabfst = false; ?>">{{ $s->name }}</span>
+                                @else
+                                    <span class="tab-trigger js-tab-trigger" data-id="tab0<?php echo $s->id ?>">{{ $s->name }}</span>
+                                @endif
+                            @endforeach
+                            <?php $tabfst = true; ?>
                         </div>
                     </div>
                 </div>
@@ -31,24 +66,20 @@ $check = 0;
                     <aside class="sidemenu sidemenu_left">
                         <h2>オブジェクト</h2>
                             <button>
-                                <div class="row" onclick="ObjBtnClick(event)"><span class="col-lg-4 ic ic_btn">BUTTON</span><span class="col-lg-8 obj_name">Button</span></div>
-                                <input type="hidden" id="id_btn" value="<?php echo $btn; $btn++;?>">
+                                <div class="row dragitem" id="drop_btn" ondragend="DragAddEnd(event)" ondragstart="DrapAddStart(event)" draggable="true" onclick="ObjBtnClick(event)"><span class="col-lg-4 ic ic_btn">BUTTON</span><span class="col-lg-8 obj_name">Button</span></div>
+                                <input type="hidden" id="id_new" value="<?php echo $newid; $newid++;?>">
                             </button>
                             <button>
-                                <div class="row" onclick="ObjRadioClick(event)"><span class="col-lg-4 ic ic_radio">●</span><span class="col-lg-8 obj_name">RadioButton</span></div>
-                                <input type="hidden" id="id_radio" value="<?php echo $radio; $radio++;?>">
+                                <div class="row dragitem" id="drop_radio" ondragend="DragAddEnd(event)" ondragstart="DrapAddStart(event)" draggable="true" onclick="ObjRadioClick(event)"><span class="col-lg-4 ic ic_radio">●</span><span class="col-lg-8 obj_name">RadioButton</span></div>
                             </button>
                             <button>
-                                <div class="row" onclick="ObjTextBoxClick(event)"><span class="col-lg-4 ic ic_textb">abc |</span><span class="col-lg-8 obj_name">TextBox</span></div>
-                                <input type="hidden" id="id_text" value="<?php echo $text; $text++;?>">
+                                <div class="row dragitem" id="drop_textbox" ondragend="DragAddEnd(event)" ondragstart="DrapAddStart(event)" draggable="true" onclick="ObjTextBoxClick(event)"><span class="col-lg-4 ic ic_textb">abc |</span><span class="col-lg-8 obj_name">TextBox</span></div>
                             </button>
                             <button>
-                                <div class="row" onclick="ObjLabelClick(event)"><span class="col-lg-4 ic ic_label">label</span><span class="col-lg-8 obj_name">Label</span></div>
-                                <input type="hidden" id="id_label" value="<?php echo $label; $label++;?>">
+                                <div class="row dragitem" id="drop_label" ondragend="DragAddEnd(event)" ondragstart="DrapAddStart(event)" draggable="true" onclick="ObjLabelClick(event)"><span class="col-lg-4 ic ic_label">label</span><span class="col-lg-8 obj_name">Label</span></div>
                             </button>
                             <button>
-                                <div class="row" onclick="ObjCheckBoxClick(event)"><span class="col-lg-4 ic ic_checkb"><label class="fas fa-check"></label></span><span class="col-lg-8 obj_name">CheckBox</span></div>
-                                <input type="hidden" id="id_check" value="<?php echo $check; $check++;?>">
+                                <div class="row dragitem" id="drop_checkbox" ondragend="DragAddEnd(event)" ondragstart="DrapAddStart(event)" draggable="true" onclick="ObjCheckBoxClick(event)"><span class="col-lg-4 ic ic_checkb"><label class="fas fa-check"></label></span><span class="col-lg-8 obj_name">CheckBox</span></div>
                             </button>
 
                             <p>
@@ -61,11 +92,14 @@ $check = 0;
                             <p>
                             < scenes ><br />
                             @foreach( $scenes_data as $s )
-                            {{ $s->name }}<br />
+                            {{ $s->id }}{{ $s->name }}<br />
                                 @foreach( $decorations_data[$s->id] as $d )
-                                - {{ $d->text }}< {{ $d->element->name }} ><br />
+                                - {{ $d->id }}-{{ $d->text }}< {{ $d->element->name }} ><br />
+                                - {{ $d->width }} - {{ $d->height }} <br />
+                                - {{ $d->position_x }} - {{ $d->position_y }} <br />
                                 @endforeach
                             @endforeach
+                            
                             </p>
                     </aside>
                 </div>
@@ -74,16 +108,20 @@ $check = 0;
         <div class="col-lg-8">
             <div class="tab-content">
                 <!-- 画面一覧の表示 -->
-                <div id="tab01" class="tab-content__item js-tab-target is-active">
+                @foreach( $scenes_data as $s )
+                @if($tabfst == true)
+                    <div id="tab0<?php echo $s->id; $tabfst = false;?>" class="tab-content__item js-tab-target is-active">
+                        <div id="drop" class="canvas" ondragover="DragOver(event)" ondrop="Drop(event)" >
+                        {{ $s->name }}</div>
+                    </div>
+                @else
+                    <div id="tab0<?php echo $s->id;?>" class="tab-content__item js-tab-target">
+                        <div id="drop" class="canvas" ondragover="DragOver(event)" ondrop="Drop(event)" >
+                        {{ $s->name }}</div>
+                    </div>
+                @endif
+                @endforeach
 
-                    <div id="drop" class="canvas" ondragover="DragOver(event)" ondrop="Drop(event)" >
-                    </div>
-                </div>
-                <!-- 画面遷移が表示される -->
-                <div id="tab02" class="tab-content__item js-tab-target">
-                    <div id="drop" class="canvas" ondragover="DragOver(event)" ondrop="Drop(event)" >
-                    </div>
-                </div>
             </div>
         </div>
         <div class="col-lg-2 ">
@@ -104,6 +142,17 @@ $check = 0;
                     <hr class="col-lg-12 sidemenu_hr">
                     <span class="col-lg-3">座標X</span><div class="v_line_fix"></div><input class="col-lg-2 js-input-elm" id="obj-x" type="text" onchange="ChangeText(event)">
                     <span class="col-lg-3">座標Y</span><div class="v_line_fix"></div><input class="col-lg-2 js-input-elm" id="obj-y" type="text" onchange="ChangeText(event)">
+                </div>
+                <div class="objectlist ac_menu" id="obj_lists">
+                    <span>追加機能一覧</span>
+                    <div class="menu construction">
+                        @foreach( $scenes_data as $s )
+                            <label class="ac_title" id="menu_<?php echo $s->id;?>" for="menu_bar<?php echo $s->id;?>">{{ $s->name }}</label>
+                            <input type="checkbox" id="menu_bar<?php echo $s->id;?>" class="accordion checked" onclick="Check(event)"/>
+                            <ul id="links<?php echo $s->id;?>">
+                            </ul>
+                        @endforeach
+                    </div>
                 </div>
             </aside>
         </div>
